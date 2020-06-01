cd044cef132ff911d2c38a0872c16e02fcdbec2f
Dylan Docherty <dylandocherty98@gmail.com>
2020-06-01 14:04:20 -0400

    - updated css and some react

--- monster-rolodex/src/App.js
+++ monster-rolodex/src/App.js
@@ -1,23 +1,33 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+
+import {CardList} from './components/card-list/card-list.component'
+
+
 import './App.css';
 
 class App extends Component {
   constructor(){
     super();
-
+    // http://jsonplaceholder.typicode.com/users
     this.state = {
-      string: "Hello Dylan"
+      monsters: []
     };
   }
+
+  componentDidMount(){
+    fetch('http://jsonplaceholder.typicode.com/users')
+    .then(response =>response.json())
+    .then(users=>this.setState({monsters: users}));
+  }
+
   render(){
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-            <p>{this.state.string}</p>
-            <button onClick={()=> this.setState({string: "Hello Austin"})}>change test</button>
-        </header>
+        <CardList>
+        {
+          this.state.monsters.map(monster=> <h1>{monster.name}</h1>)
+        }
+        </CardList>
       </div>
     );   
   }
