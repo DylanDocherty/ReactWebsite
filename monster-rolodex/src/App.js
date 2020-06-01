02df7f92619a566590a0ec4305fc4c3fb9646c29
Dylan Docherty <dylandocherty98@gmail.com>
2020-06-01 15:10:25 -0400

    - add another component "card" to further simplify

--- monster-rolodex/src/App.js
+++ monster-rolodex/src/App.js
@@ -23,11 +23,7 @@ class App extends Component {
   render(){
     return (
       <div className="App">
-        <CardList>
-        {
-          this.state.monsters.map(monster=> <h1>{monster.name}</h1>)
-        }
-        </CardList>
+        <CardList monsters={this.state.monsters}></CardList>
       </div>
     );   
   }
